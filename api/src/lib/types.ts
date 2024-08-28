ad2ffefa92e3defdf70b9cf05fac3c0430179222
RamonHA13 <ramon.hernandez5086@alumnos.udg.mx>
2024-08-28 00:49:57 -0600

    update: libraries

--- api/src/lib/types.ts
+++ api/src/lib/types.ts
@@ -1,11 +1,12 @@
 export type ReturnTuple<T> = [unknown | Error | null, T | null]
+export type uuid = ReturnType<typeof crypto.randomUUID>
 
 declare module 'express-serve-static-core' {
   interface Request {
     session: {
       user?: {
         id: string
-        role: string
+        role: string[]
       }
     }
   }
